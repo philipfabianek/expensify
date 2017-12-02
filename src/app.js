4c2d159410061091b701ace0948617efffb4d511
philipfabianek <fabianekphilip@gmail.com>
2017-12-02 19:27:34 +0100

    Removed static expense data from app.js.

--- src/app.js
+++ src/app.js
@@ -8,19 +8,8 @@ import "react-dates/lib/css/_datepicker.css";
 
 import AppRouter from "./routers/AppRouter";
 import configureStore from "./store/configureStore";
-import getVisibleExpenses from "./selectors/expenses";
-import { addExpense, removeExpense, editExpense } from "./actions/expenses";
-import { setTextFilter, sortByDate, sortByAmount, setStartDate, setEndDate } from "./actions/filters";
 
 const store = configureStore();
-
-store.dispatch(addExpense({ description: "Water bill", amount: 68000, createdAt: 1150, note: "ayy" }));
-store.dispatch(addExpense({ description: "Gas bill", amount: 300000, createdAt: 950 }));
-store.dispatch(addExpense({ description: "Rent", amount: 198750, createdAt: 850 }));
-
-// const state = store.getState();
-// const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-// console.log(visibleExpenses);
 
 const jsx = (
     <Provider store={store}>
